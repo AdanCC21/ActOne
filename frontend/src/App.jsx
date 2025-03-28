3db178316a5ec5a02f5e4793ab3e07150048451e
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2025-03-28 00:33:28 -0700

    ngix work, but don't show the front page, i think becaouse it doesn't build the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    fetch("http://localhost:3000/api/story-hello")
-      .then(res => res.json())
-      .then(data => setUser(data));
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/api/story-hello")
+  //     .then(res => res.json())
+  //     .then(data => setUser(data));
+  // }, []);
+
+  // {user ? <h1 className="text-green-400">Message: {user.message}</h1> : <p className="text-red-600">Loading...</p>}
 
   return (
     <div className="flex flex-col w-screen h-screen justify-center items-center">
-      {user ? <h1 className="text-green-400">Message: {user.message}</h1> : <p className="text-red-600">Loading...</p>}
+      <h1>Hello World :D</h1>
     </div>
   );
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  build:{
+    outDir:'dist'
+  },
   plugins: [react(),tailwindcss()],
 })
