7fc7bb348283cacc15bff7762dd3b7105fbfb329
Andan <141715477+AdanCC21@users.noreply.github.com>
2025-04-28 08:30:14 -0700

    things in the frontend

--- frontend/src/pages/Edit.jsx
+++ frontend/src/pages/Edit.jsx
@@ -5,9 +5,37 @@ import boldFormat from '../assets/editorIcons/bold.svg';
 import italicFormat from '../assets/editorIcons/italic.svg';
 import underlineFormat from '../assets/editorIcons/underline.svg';
 import '../css/edit.css'
+import { act, useState } from "react";
+
+class Act {
+    constructor(id) {
+        this.id = id;
+        this.title = "";
+        this.content = "";
+    }
+}
+
+
 
 export default function Edit({ }) {
     const { title } = useParams();
+    const [act, setAct] = useState([new Act(0), new Act(1)]);
+    const [currentAct, setCurrent] = useState(0);
+
+    const handleChanges = (e) => {
+        const { name, value } = e.target;
+
+        setAct(prev => prev.map((current, index) => (index === currentAct ? (
+            { ...prev, [name]: value }
+        ) : (
+            { ...prev }
+        ))))
+    }
+
+    const move = (index) => {
+        setCurrent(index)
+    }
+
     return (
         <div className="overflow-hidden">
             <Header></Header>
@@ -15,10 +43,9 @@ export default function Edit({ }) {
                 <div className="e-acts">
                     <h4>Actos</h4>
                     <ul>
-                        <li>Acto 1</li>
-                        <li>Acto 2</li>
-                        <li>Acto 3</li>
-                        <li>Acto 4</li>
+                        {act.forEach((current, index) => (
+                            <li>{current.title}</li>
+                        ))}
                     </ul>
                 </div>
 
@@ -26,16 +53,26 @@ export default function Edit({ }) {
                     <div className="e-editor-header">
                         <h2>{title}</h2>
                         <div className="flex ml-auto mt-auto">
-                            <img className="mr-2" src={titleFormat}/>
-                            <img className="mx-2" src={boldFormat}/>
-                            <img className="mx-2" src={italicFormat}/>
-                            <img className="ml-2" src={underlineFormat}/>
+                            <img className="mr-2" src={titleFormat} />
+                            <img className="mx-2" src={boldFormat} />
+                            <img className="mx-2" src={italicFormat} />
+                            <img className="ml-2" src={underlineFormat} />
                         </div>
                     </div>
 
                     <section className="e-editor-st-area">
-                        <h4 className="text-(--red-500) ml-2 my-1">Titulo del acto</h4>
-                        <textarea className="w-full" />
+                        <input
+                            name="title"
+                            value={act[currentAct].title}
+                            placeholder="Titulo para tu acto"
+                            onChange={(e) => { handleChanges(e) }}
+                            className="text-(--red-500)" />
+                        <textarea
+                            name="content"
+                            value={act[currentAct].content}
+                            placeholder="Write your act"
+                            onChange={(e) => { handleChanges(e) }}
+                            className="w-full" />
                     </section>
                 </div>
 
