46d544ee6e51fa8b7fc8ef2dcddbf898fd3536af
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2025-05-10 02:27:28 -0700

    backend, likes and comments services done, but i dont have controllers or routes to test it

--- backend/public-data/src/app.service.ts
+++ backend/public-data/src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { BlobOptions } from 'buffer';
 
 @Injectable()
 export class AppService {
@@ -33,4 +34,50 @@ export class AppService {
       }
     }
   }
+
+  async SetLike(id: number, pubId: number, pubType: string, userId: number, state: boolean) {
+    try {
+      if (!id) {
+        const newLike = await this.prismaSer.likes.create({
+          data: {
+            user_id: userId,
+            publication_id:pubId,
+            publication_type:pubType
+          }
+        })
+        return newLike;
+      }else{
+        const res = await this.prismaSer.likes.update({ where: { id: id }, data: { state: !state } })
+        return res;
+      }
+    } catch (e) {
+      console.error(e.message);
+      return e.message
+    }
+  }
+
+  async AddComment(userId: number, publicationId: number, content: string) {
+  try {
+    const newComment = await this.prismaSer.comments.create({
+      data: {
+        user_id: userId,
+        publication_id: publicationId,
+        content: content
+      }
+    });
+
+    return {
+      message: 'Comment added successfully',
+      data: newComment
+    };
+  } catch (e) {
+    console.error('AddComment error:', e.message);
+    return {
+      message: e.message,
+      data: null
+    };
+  }
+}
+
+  
 }
